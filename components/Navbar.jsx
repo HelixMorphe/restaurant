6e8618bf236c20bec64073cc080fbb3d608bbc71
Skillcet <skillsntools.web@gmail.com>
2022-02-03 17:19:14 +0530

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,6 @@
 import styles from "../styles/Navbar.module.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 function Navbar() {
   return (
     <div className={styles.container}>
@@ -7,9 +9,27 @@ function Navbar() {
           <div className={styles.text}>ORDER NOW</div>
           <div className={styles.text}>995-905-9501</div>
         </div>
+        {/* <div className={styles.logo}>
+          <PhoneEnabledIcon fontSize="large" />
+        </div> */}
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <li className={styles.listItemTitle}>DHANDA</li>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <ShoppingCartIcon fontSize="large" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 }
