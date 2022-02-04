9f4292ce9492314b41a45d2ac90abf83431c1651
Skillcet <skillsntools.web@gmail.com>
2022-02-05 02:31:49 +0530

    carousel 2

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import styles from "../styles/CartItem.module.css";
+import Image from "next/image";
 function CartItem() {
-  return <div className={styles.container}>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/img/11.png"
+          alt="pizza"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <h2 className={styles.title}>Cheese Burst Pizza</h2>
+    </div>
+  );
 }
 
 export default CartItem;
